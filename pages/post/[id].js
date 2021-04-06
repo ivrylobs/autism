d672f12a5efa62dfc2e94af5ebb833b444a0919c
ivrylobs <mt.f.rk.shit@gmail.com>
2021-04-06 18:14:03 +0700

    Add Dynamically SSG

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,20 +1,32 @@
 import axios from "axios";
+import { fetchAPI } from "../../lib/api";
+import ReactMarkdown from "react-markdown";
 
 const instance = axios.create({
   baseURL: "http://localhost:1337",
 });
 
 const Post = ({ post }) => {
-  return <div>Test</div>;
+  return (
+    <div>
+      Test {post.id}
+      <ReactMarkdown
+        transformImageUri={(uri) => {
+          console.log(uri);
+          return "http://localhost:1337" + uri;
+        }}
+        children={post.content}
+      />
+    </div>
+  );
 };
 
 export async function getStaticPaths() {
-  const blogs = await instance.get("/blogs");
-  console.log(blogs);
+  const blogs = await fetchAPI("/blogs");
   return {
-    paths: blogs.map((item) => ({
+    paths: blogs.map((blog) => ({
       params: {
-        id: item.id,
+        id: blog.id.toString(),
       },
     })),
     fallback: false,
@@ -22,12 +34,11 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const post = await instance.get("/blogs/1");
-  console.log(post);
+  const post = await instance.get(`/blogs/${params.id}`);
 
   return {
     props: {
-      post: post,
+      post: post.data,
     },
   };
 }
